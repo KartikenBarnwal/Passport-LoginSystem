98ec6e5a4580d04542d59da81ac0615b386efbe8
Niranjan Kumar <kartikenkrb@gmail.com>
2020-07-19 03:40:51 +0530

    env file

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGO_Connect,{
+mongoose.connect('mongodb+srv://'+process.env.DB_USER+':'+process.env.DB_PASS+'@investocks-8camt.mongodb.net/users',{
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
